3882c595460d490f3da5ac29f85be4f55d808e4e
danielcaldeir <danielcaldeir@gmail.com>
2024-02-22 16:09:10 -0300

    colocando o projeto no Vercel

---
+++ public/server.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const http_1 = __importDefault(require("http"));
+const https_1 = __importDefault(require("https"));
+const site_1 = __importDefault(require("./routes/site"));
+const admin_1 = __importDefault(require("./routes/admin"));
+const requestIntercepter_1 = require("./utils/requestIntercepter");
+const fs_1 = __importDefault(require("fs"));
+// import { server } from 'typescript';
+// import router from './routes/site';
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.all('*', requestIntercepter_1.requestIntercepter);
+app.use('/admin', admin_1.default);
+app.use('/', site_1.default);
+const runServer = (port, server) => {
+    server.listen(port, () => {
+        console.log("Running at PORT " + port);
+    });
+};
+const regularServer = http_1.default.createServer(app);
+if (process.env.NODE_ENV === 'production') {
+    // TODO: configurar SSL
+    // TODO: rodar sever na 80 e na 443
+    const options = {
+        key: fs_1.default.readFileSync(process.env.SSL_KEY),
+        cert: fs_1.default.readFileSync(process.env.SSL_CERT)
+    };
+    const secServer = https_1.default.createServer(options, app);
+    runServer(80, regularServer);
+    runServer(443, secServer);
+}
+else {
+    const serverPort = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+    runServer(serverPort, regularServer);
+}
